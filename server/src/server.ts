07a36d2e137db435a5e9b4ee28ae699db38ae923
felixHerder <felixbaciu@gmail.com>
2021-10-21 14:38:50 +0300

    server .env

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import http from "http";
 import appToServe from "./app";
 import mongoose from "mongoose";
-
 import { loadPlanetsData } from "./models/planets.model";
 import {loadLaunchData} from './models/launches.model';
 import { loadRocketData } from "./models/rockets.model";
 import { loadLaunchpadData } from "./models/launchpads.model";
 
 const PORT = process.env.PORT || 8000;
-const MONGO_URL = "***REMOVED***?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL as string;
+console.log(process.env.MONGO_URL);
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
